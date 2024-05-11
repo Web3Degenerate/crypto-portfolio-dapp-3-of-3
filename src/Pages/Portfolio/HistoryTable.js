6cf97adaddbf11015a9f0bf6b392a7c818b8271f
hostnomics <hostnomics@gmail.com>
2024-05-11 15:06:54 -0500

    Added history useState and fetchHistory function to make a useEffect call when the page loads

--- src/Pages/Portfolio/HistoryTable.js
+++ src/Pages/Portfolio/HistoryTable.js
@@ -5,12 +5,47 @@ import React, {useEffect, useState } from 'react';
 // function HistoryTable( {selectedChains, address} ){
 function HistoryTable( {address, selectedChains} ){
 
+//add state logic to save the history of a given wallet:
+    const [history, setHistory] = useState([])
+
+    function fetchHistory(chain, fromDate, address){
+      const baseUrl = `https://deep-index.moralis.io/api/v2.2/wallets`;
+      // const queryParams = `?chain=${chain}&from_date=${fromDate}&order=DESC&nft_metadata=true`
+      const queryParams = `?chain=${chain}&from_date=${fromDate}&order=DESC`;
+      const url = `${baseUrl}/${address}/history${queryParams}`;
+
+      const headers = {
+        "Content-Type": "application/json",
+        "X-API-Key": process.env.REACT_APP_MORALIS_API_KEY,
+      }
+
+      return fetch(url, {headers})
+      .then(data => {
+        console.log(data);
+      })
+    }
+
+
+//Put this inside useEffect to run when page loads (17:42): https://youtu.be/noNyHV9HPM8?si=gng0rMfJni08UZgO&t=1062 
+    useEffect( () => {
+      //set date 30 days from current date: 
+      const today = new Date();
+      const fromDate = new Date(today.setDate(today.getDate() - 30));
+
+      //call fetchHistory and hardcode chain for now
+      fetchHistory("eth", fromDate)
+
+    }, [address, selectedChains]) //we have dependency here on address and selectedChains
+
     return (
       <div>
         History
       </div>
+
     )
 
 }
+
+// Start at (10:24) adding the logic to fetch the history: https://youtu.be/noNyHV9HPM8?si=l0Q1NKqliyEg5lgj&t=624
 
 export default HistoryTable;
